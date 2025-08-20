f3af71b5df20da21a8ad4bf27d787c2a5a6e42d5
Amir Hashmi <amirhashmi017@gmail.com>
2025-08-20 11:37:46 +0500

    Data Transformation Architecture

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -2,8 +2,8 @@ import yaml
 from networksecurity.exception.exception import NetworkSecuityException
 from networksecurity.logging.logger import logging
 import os,sys
-import dill
 import pickle
+import numpy as np
 
 def read_yaml_file(file_path: str)->dict:
     try:
@@ -20,5 +20,23 @@ def write_yaml_file(filepath:str,content:object,replace:bool= False)->None:
         os.makedirs(os.path.dirname(filepath),exist_ok=True)
         with open(filepath,'w') as file:
             yaml.dump(content,file)
+    except Exception as e:
+        raise NetworkSecuityException(e,sys)
+
+def save_numpy_array_data(filepath,array:np.array):
+    try:
+        dir_path=os.path.dirname(filepath)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(filepath,'wb') as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise NetworkSecuityException(e,sys)
+    
+def save_object(filepath,obj):
+    try:
+        dir_path=os.path.dirname(filepath)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(filepath,'wb') as file_obj:
+            pickle.dump(obj,file_obj)
     except Exception as e:
         raise NetworkSecuityException(e,sys)
