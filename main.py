7c84c1a6611adb2db8baee9c062cbb3ace52de09
Amir Hashmi <amirhashmi017@gmail.com>
2025-08-20 10:50:41 +0500

    Data Validation Component Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import TrainingPieplineConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecuityException
 from networksecurity.logging.logger import logging
 
@@ -9,12 +10,19 @@ import sys
 
 if __name__=="__main__":
     try:
-        training_pipeline_config= TrainingPieplineConfig()
+        training_pipeline_config= TrainingPipelineConfig()
         data_ingestion_config=DataIngestionConfig(training_pipeline_config)
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("Initiate Data Ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
         print(data_ingestion_artifact)
+        logging.info(" Data Initiation Completed")
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info(" Initiate Data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        logging.info("Data Validation Completed")
 
     except Exception as e:
         raise NetworkSecuityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -11,3 +11,14 @@ def read_yaml_file(file_path: str)->dict:
             return yaml.safe_load(yaml_file) 
     except Exception as e:
         raise NetworkSecuityException(e,sys)
+
+def write_yaml_file(filepath:str,content:object,replace:bool= False)->None:
+    try:
+        if replace:
+            if os.path.exists(filepath):
+                os.remove(filepath)
+        os.makedirs(os.path.dirname(filepath),exist_ok=True)
+        with open(filepath,'w') as file:
+            yaml.dump(content,file)
+    except Exception as e:
+        raise NetworkSecuityException(e,sys)
